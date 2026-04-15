99c518cfb143a9f5d163464947008fb4041b3eef
Shahad  AL Housni <shahadhamed1820@gmail.com>
2026-04-15 15:23:46 +0400

    Add files via upload

---
+++ solution5/project01/Program.cs
@@ -0,0 +1,146 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Task 1 - Day Name Printer
+
+
+            // Console.WriteLine("Enter a number from 1 to 7 : ");
+            //int day = Convert.ToInt32 (Console.ReadLine());
+
+            // #region switch 
+            // switch (day)
+            //    {
+            //        case 1:
+            //            Console.WriteLine("Monday");
+            //            break;
+            //        case 2:
+            //            Console.WriteLine("Tuesday");
+            //            break;
+            //        case 3:
+            //            Console.WriteLine("Wednesday");
+            //            break;
+            //        case 4:
+            //            Console.WriteLine("Thursday");
+            //            break;
+            //        case 5:
+            //            Console.WriteLine("Friday");
+            //            break;
+            //        case 6:
+            //            Console.WriteLine("Saturday");
+            //            break;
+            //        case 7:
+            //            Console.WriteLine("Sunday");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Invalid day number");
+            //            break;
+            //    }
+            //#endregion
+
+
+
+
+            //-----------------------------------------------------------
+            //Task 2 – Multiplication Table
+
+
+            //Console.WriteLine("Enter a number: ");
+            //int num = Convert.ToInt32(Console.ReadLine());
+
+
+            //for (int i = 1; i <= 10; i++)
+            //{
+            //    Console.WriteLine(num + " x " + i + " = " + (num * i));
+            //}
+
+
+
+
+            //-----------------------------------------------------------
+            //Task 3 – Countdown Timer
+
+
+            //Console.WriteLine("Enter a positive number: ");
+            //int number = Convert.ToInt32(Console.ReadLine());
+
+
+            //if (number <= 0) /*Not Positive*/
+            //{
+            //    Console.WriteLine("Please enter a positive number");
+            //}
+            //else
+            //{
+            //    while (number >= 1)
+            //    {
+            //        Console.WriteLine(number);
+            //        number = number - 1;
+            //    }
+            //    Console.WriteLine("Go!");
+            //}
+
+
+
+            //-----------------------------------------------------------
+            //Task 4 – Season Detector
+
+
+            //Console.WriteLine("Enter month number (1-12): ");
+            //int month = Convert.ToInt32(Console.ReadLine());
+
+            //#region switch 
+            //switch (month)
+            //{
+            //    case 12:
+            //    case 1:
+            //    case 2:
+            //        Console.WriteLine("Winter");
+            //        break;
+
+            //    case 3:
+            //    case 4:
+            //    case 5:
+            //        Console.WriteLine("Spring");
+            //        break;
+
+            //    case 6:
+            //    case 7:
+            //    case 8:
+            //        Console.WriteLine("Summer");
+            //        break;
+
+            //    case 9:
+            //    case 10:
+            //    case 11:
+            //        Console.WriteLine("Autumn");
+            //        break;
+
+            //    default:
+            //        Console.WriteLine("Invalid month number");
+            //        break;
+            //}
+            //#endregion
+
+
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+} 
+    
+
