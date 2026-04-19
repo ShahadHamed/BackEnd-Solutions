171ec63f14026fe91dfe08d9c103776eda128129
Shahad  AL Housni <shahadhamed1820@gmail.com>
2026-04-19 14:42:03 +0400

    Add files via upload

---
+++ solution7/project01/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace project01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*=======================Erorr types========================*/
+
+            #region Error types
+
+            //1. Syntax error
+            //2. Runtime Errors
+            //3. Logic Errors
+
+
+            //Console.WriteLine("Enter first number :");
+            //int num1= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second number :");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num1/num2);
+
+
+            //--------------------------------------------------------------
+
+            //string name = null;
+            //Console.WriteLine(name.Length);
+
+
+            //--------------------------------------------------------------
+
+            //Console.WriteLine("Enter first number :");
+            //int num1= Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter second number :");
+            //int num2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine(num1 - num2);
+
+            #endregion
+
+
+            /*=======================Erorr Handles========================*/
+
+            #region ERROR HANDLE
+
+            //try
+            //{
+            //    int num = int.Parse("shahad");
+            //    string name = null;
+            //    Console.WriteLine(name.Length);
+            //}
+            //catch
+            //{
+            //    Console.WriteLine("Error has been shown");
+
+            //}
+
+
+            //--------------------------------------------------------------
+
+
+            //try
+            //{
+            //    int num = int.Parse("shahad");
+            //}
+            //catch (Exception x)
+            //{
+            //    Console.WriteLine(x.Message);
+            //}
+
+            //--------------------------------------------------------------
+
+            //Console.WriteLine("Entet any number:");
+            //int x = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Entet any number:");
+            //int y = Convert.ToInt32(Console.ReadLine());
+
+            //try
+            //{
+            //    Console.WriteLine(x / y);
+            //}
+            //catch (Exception shosh)
+            //{
+            //    Console.WriteLine(shosh.Message);
+            //}
+            //finally
+            //{
+            //    int result = Convert.ToInt32(Console.ReadLine());
+            //    int final = result / y;
+            //    Console.WriteLine (final);
+            //}
+
+
+            //--------------------------------------------------------------
+          
+        }
+    }
+}
